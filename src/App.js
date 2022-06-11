f12c76ab624352846257e533f7a99564ff485aec
SM Surid <maruf15-9549@diu.edu.bd>
2022-06-11 17:24:59 +0600

    Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
     return (
-        <h1>Hello from the App</h1>
+        <>
+            <Header text={true} />
+            <div className="container">
+                <h1>Hello from the App</h1>
+            </div>
+        </>
     )
 }
 export default App;
