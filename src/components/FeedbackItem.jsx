8befcb302f51818edc1c5fa887bf08b0e7379ef5
SM Surid <maruf15-9549@diu.edu.bd>
2022-06-13 18:02:21 +0600

    Rating Added

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,18 @@
+import { FaTimes} from 'react-icons/fa'
 import Card from "./shared/Card"
-
+import PropTypes from 'prop-types'
 
 function FeedbackItem({item}) {
-  
+  const handleClick=(id)=>{
+    console.log(id)
+  }
   return (
     <Card reverse={true}>
         <div className="num-display">{item.rating}
         </div>
+        <button onClick={()=> handleClick(item.id)} className="close">
+          <FaTimes color='purple'/>
+        </button>
             <div className="text-display">
                 {item.text}
             </div>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 import feedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 
 function FeedbackList({feedback}) {
     if(!feedback || feedback.length === 0) {
@@ -15,14 +16,14 @@ function FeedbackList({feedback}) {
   )
 }
 
-// FeedbackList.propTypes = {
-//     feedback: PropTypes.arrayOf(
-//       PropTypes.shape({
-//         id: PropTypes.number.isRequired,
-//         rating: PropTypes.number.isRequired,
-//         text: PropTypes.string.isRequired,
-//       })
-//     ),
-// }
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+      })
+    ),
+}
 
 export default FeedbackList
