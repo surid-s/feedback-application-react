10f182f8d71636c6d715984b23a1a1316335a4be
SM Surid <maruf15-9549@diu.edu.bd>
2022-06-13 17:01:33 +0600

    List Added

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react'
+import Card from "./shared/Card"
 
-function FeedbackItem() {
-    const[rating, setRating]= useState(7);
-    const[text, setText] = useState('This is an example of feedback');
 
+function FeedbackItem({item}) {
+  
   return (
-    <div className="card">
-        <div className="num-display">{rating}
+    <Card reverse={true}>
+        <div className="num-display">{item.rating}
         </div>
             <div className="text-display">
-                {text}
+                {item.text}
             </div>
-    </div>
+    </Card>
   )
 }
 
